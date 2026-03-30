90810165f54f26fc7e664830f45e6cd0993446a2
tapatsawin <128007324+tapatsawin@users.noreply.github.com>
2026-03-30 08:42:54 +0000

    add 2 file

--- main.cpp
+++ main.cpp
@@ -10,6 +10,12 @@ using namespace std;
 #include "monster.h"
 
 int main(int argc, const char * argv[]) {
- 
+    monster *p;
+
+    monster m1("Shrim ",10,1);
+    monster m2("Act "),m4("Pong ",30);
+    monster x[5] = {{"John ",5},{"GG ",10,20}};
+    p=new monster("Lulu",10,50);
+    delete p;
     return 0;
 }
--- monster.h
+++ monster.h
@@ -10,12 +10,28 @@
 class monster{
 private:
     string name;
-    int hp,potion;
+    unsigned int hp,potion;
 public:
     void Attack(monster &);
-          void heal();
-    monster(string, int,int);
+    void heal();
+    monster(string="default ",int=10,int=1);
+    ~monster();
 };
 
+
+monster :: ~monster()
+{
+    cout<<"Goooooo "<< name <<"hp: "<<hp<<endl;
+}
+
+monster::monster(string n,int h,int p)
+{
+    name = n;
+    h>200?hp=200:hp=h;
+    p>10?potion=10:potion=p;
+
+    cout<<"Monster "<<name <<"coming soon"<<endl;
+
+}
 
 #endif /* monster_h */
